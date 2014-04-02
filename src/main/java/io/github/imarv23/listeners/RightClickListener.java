63db1d44f4b219d58ab80a77e2cd65efafbd6afd
iMarv <git.imarv@web.de>
2014-04-02 15:47:40 +0200

    Added config support
    pigs now use the config file

--- src/main/java/io/github/imarv23/listeners/RightClickListener.java
+++ src/main/java/io/github/imarv23/listeners/RightClickListener.java
@@ -4,6 +4,7 @@ import io.github.imarv23.spawning.PigSpawner;
 
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,7 +15,12 @@ public class RightClickListener implements Listener {
 	
 	private Player p;
 	private Block b;
+	private FileConfiguration config;
 	
+	public RightClickListener(FileConfiguration config) {
+		this.config = config;
+	}
+
 	@EventHandler
 	public void onPlayerInteract(PlayerInteractEvent e)
 	{
@@ -23,9 +29,9 @@ public class RightClickListener implements Listener {
 			this.p = e.getPlayer();
 			this.b = e.getClickedBlock();
 			
-			if(this.getItemInHand(this.p) == Material.PORK)
+			if(this.getItemInHand(this.p) == Material.PORK && this.p.getItemInHand().getAmount() >= config.getInt("Pig.Sacrifice"))
 			{
-				PigSpawner pig = new PigSpawner(this.p);
+				PigSpawner pig = new PigSpawner(this.p, config.getInt("Pig.Sacrifice"));
 				
 				if(pig.checkForValidStructure(this.b, Material.HAY_BLOCK))
 				{
--- src/main/java/io/github/imarv23/spawning/PigSpawner.java
+++ src/main/java/io/github/imarv23/spawning/PigSpawner.java
@@ -1,21 +1,32 @@
 package io.github.imarv23.spawning;
 
+import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public class PigSpawner extends RightClickSpawner {
 	
-	public PigSpawner(Player p)
+	private int sacrifice;
+	
+	public PigSpawner(Player p, int s)
 	{
 		super(p);
+		this.sacrifice = s;
 	}
 	
 	public void spawnPig()
 	{
 		if(this.struct != null)
 		{
-			this.pl.getWorld().spawnEntity(this.struct.getSpawningLocation() , EntityType.PIG);
 			this.struct.removeBlocks();
+			this.pl.getWorld().spawnEntity(this.struct.getSpawningLocation() , EntityType.PIG);
+			this.paySacrifice();
 		}
+	}
+	
+	private void paySacrifice()
+	{
+		this.pl.getInventory().removeItem(new ItemStack(Material.PORK, this.sacrifice));
 	}
 }
